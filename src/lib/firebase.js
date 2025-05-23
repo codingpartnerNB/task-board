3492d387be883feefd0fe0cb8baa649cb9bf499e
Neha Bisht <nehab5314@gmail.com>
2025-05-23 21:24:17 +0530

    Some fixes

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -6,14 +6,14 @@ import { getDatabase } from 'firebase/database';
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA7pW26pwKXcQVQRzmXK7ifVpbMwIKdYek",
-  authDomain: "task-board-e2c1d.firebaseapp.com",
-  projectId: "task-board-e2c1d",
-  storageBucket: "task-board-e2c1d.firebasestorage.app",
-  messagingSenderId: "211210338431",
-  appId: "1:211210338431:web:01067931ab93e1f8c43f4a",
-  measurementId: "G-EMHFTR0F1Q",
-  databaseURL: "https://task-board-e2c1d-default-rtdb.firebaseio.com"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
+  databaseURL: import.meta.env.VITE_DATABASE_URL
 };
 
 // Initialize Firebase
